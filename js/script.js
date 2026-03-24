18fa72781a9d3f929627941e826bbea42eb98691
branAlmoraM <brandonalmora@gmail.com>
2026-03-23 21:34:24 -0600

    Interactividad: agrega variables para seleccionar elementos del DOM.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//Seleccionar elementos del DOM
+const cantidadSelecc = document.getElementById("tamano");
+const formatoSelecc = document.getElementById("formato");
+const boton = document.getElementById("generar");
+const contenedor = document.getElementById("paleta");
