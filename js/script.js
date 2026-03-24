d1b12db396250cac6705e1ea6c4f539d74153d54
branAlmoraM <brandonalmora@gmail.com>
2026-03-23 23:46:17 -0600

    Funcionalidad: agrega funciones para generar colores HSL y Hexa de manera aleatoria.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ const cantidadSelecc = document.getElementById("tamano");
 const formatoSelecc = document.getElementById("formato");
 const boton = document.getElementById("generar");
 const contenedor = document.getElementById("paleta");
+
+//Creación de funciones para generar colores de manera aleatoria
+function generarHSL() {
+  const h = Math.floor(Math.random() * 360); // Hue es un valor de 0 a 360
+  const s = Math.floor(Math.random() * 100); // Stauration es un valor de 0 100
+  const l = Math.floor(Math.random() * 100); // Light es un valor de 0 a 100
+
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+function generarHEX() {
+  const hex = Math.floor(Math.random() * 16777215) // 16777215 valor máximo de hexadecimales
+    .toString(16) // convierte el número a hexadecimal
+    .padStart(6, "0"); // rellena con 0 si no se completan los 6 números del formato hezadecimal
+  return "#" + hex; // retorna el color hexadecimal
+}
